b3ef9632aae1324633f692ef81186d23eb900068
danibreaddDev <danielpamblancomestre@gmail.com>
2024-08-24 16:03:28 +0200

    cambios en urls

--- src/i18n/util.js
+++ src/i18n/util.js
@@ -2,8 +2,11 @@ export function getLangFromred(url) {
     // Implement your language detection logic here
     // For example:
     const lang = url.split('/')[1];
-    if(lang===""){
-      return ""
+    if(lang==="projects"){
+      return "";
+    }
+    if (lang==="en") {
+      return "en";
     }
     return lang;
   }
